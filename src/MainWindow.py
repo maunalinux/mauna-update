6ad9d02fcbd47f8b1879d7690a1aa144ac03f772
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2024-06-02 12:47:08 -0300

    Fix quit app func

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1075,6 +1075,8 @@ class MainWindow(object):
 
     def on_ui_quitdialogyes_button_clicked(self, button):
         self.ui_quit_dialog.hide()
+        if self.about_dialog.is_visible():
+            self.about_dialog.hide()        
         self.main_window.get_application().quit()
 
     def on_ui_quitdialogno_button_clicked(self, button):
@@ -1090,6 +1092,8 @@ class MainWindow(object):
             self.ui_quit_dialog.run()
             self.ui_quit_dialog.hide()
         else:
+        if self.about_dialog.is_visible():
+            self.about_dialog.hide()        
             self.main_window.get_application().quit()
 
     def monitoring(self):
