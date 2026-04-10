43e4cef8cfa3be7c6ee3b0b60115306906338dda
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2026-04-10 12:25:56 -0300

    Add argv int control to SystemSettingsWrite

--- src/SystemSettingsWrite.py
+++ src/SystemSettingsWrite.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 16 14:53:13 2022
+
 @author: fatih
 """
 
@@ -36,12 +37,21 @@ def main():
         with open(configdir + configfile, "w") as cf:
             config.write(cf)
 
+    def validate_timestamp(value):
+        try:
+            ts = int(value)
+            if ts < 0:
+                raise ValueError
+            return str(ts)
+        except ValueError:
+            raise ValueError("Invalid timestamp")
+
     if len(sys.argv) > 2:
         if sys.argv[1] == "write":
             if sys.argv[2] == "lastupdate":
-                write_lastupdate(sys.argv[3])
+                write_lastupdate(validate_timestamp(sys.argv[3]))
             if sys.argv[2] == "lastupgrade":
-                write_lastupgrade(sys.argv[3])
+                write_lastupgrade(validate_timestamp(sys.argv[3]))
         else:
             print("unknown argument error")
     else:
